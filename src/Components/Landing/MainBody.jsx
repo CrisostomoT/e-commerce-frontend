802e5164fca043b3b165e0112fb12dc5fcc0b0d7
CrisT <crisostomotorres@hotmail.com>
2020-08-14 01:53:44 -0300

    first commit

---
+++ src/Components/Landing/MainBody.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Box, Grid, Sidebar, Header, Footer, Main, Carousel, Image } from 'grommet';
+
+const MainBody = () => {
+
+    return (
+        <Box>
+            <Main pad="large">
+                <Grid
+                    pad="small"
+                    responsive={true}
+                    align="center"
+                    alignContent="between"
+                >
+                    <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
+                        <Box flex align="center" justify="center">
+                            <Box>
+                                <Carousel>
+                                    <Image fit="cover" />
+                                </Carousel>
+                            </Box>
+                            <Box></Box>
+                            <Box></Box>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Main>
+        </Box>
+    )
+};
+
+export default MainBody;
